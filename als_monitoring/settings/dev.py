467e63f5c6cb2d848d7c46f094314b98467abd82
Chiemezuo <chiemezuo@crowdafrik.com>
2025-05-31 00:09:42 +0100

    changed sqlite to postgres

--- als_monitoring/settings/dev.py
+++ als_monitoring/settings/dev.py
@@ -10,8 +10,12 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pgdb',
+        'USER': 'chiemezuo',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
